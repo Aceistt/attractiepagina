621066bdb6294e9c7c821aa62401de546ab2822d
Aceist <79134787+Aceistt@users.noreply.github.com>
2025-05-07 13:42:44 +0200

    belp

--- admin/attracties/index.php
+++ admin/attracties/index.php
@@ -26,17 +26,57 @@ if(!isset($_SESSION['user_id']))
 <body>
 
     <?php require_once '../../header.php'; ?>
+
     <div class="container">
 
         <a href="create.php">Nieuwe attractie maken &gt;</a>
 
         <?php
-        require_once '../backend/conn.php';
-        $query = "SELECT * FROM rides";
-        $statement = $conn->prepare($query);
-        $statement->execute();
+        if (empty($_GET['status'])) 
+        {
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM rides";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            
+        }
+        elseif (!empty($_GET['status']) && ($_GET['status'] == "min_length")) 
+        {
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM rides ORDER BY min_length DESC";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+        }
+        elseif (!empty($_GET['status']) && ($_GET['status'] == "themeland")) 
+        {
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM rides ORDER BY themeland ASC";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+        }
+        else 
+        {
+            require_once '../backend/conn.php';
+            $query = "SELECT * FROM rides ORDER BY title DESC";
+            $statement = $conn->prepare($query);
+            $statement->execute();
+            
+        }
         $rides = $statement->fetchAll(PDO::FETCH_ASSOC);
+         
+        
         ?>
+        <h1>Aantal attracties in deze lijst:<?php echo count($rides) ?></h1>
+
+        <form action="" method="GET">
+            <select name="status">
+                <option value="">Kies status om te filteren</option>
+                <option value="title">Titel</option>
+                <option value="min_length">Minimale lengte</option>
+                <option value="themeland">Themagebied</option>
+            </select>      
+            <input type="submit" value="filter">   
+        </form>
 
         <table>
             <tr>
@@ -44,13 +84,23 @@ if(!isset($_SESSION['user_id']))
                 <th>Themagebied</th>
                 <th>Min. lengte</th>
                 <th>Fastpass</th>
+                <th>Aanpassen</th>
             </tr>
             <?php foreach($rides as $ride): ?>
                 <tr>
                     <td><?php echo $ride['title']; ?></td>
-                    <td><?php echo $ride['themeland']; ?></td>
+                    <td><?php echo ucfirst($ride['themeland']); ?></td>
                     <td><?php echo $ride['min_length']; ?></td>
-                    <td><?php echo $ride['fast_pass']; ?></td>
+                    <td>
+                    <?php
+                        if ($ride['fast_pass'] == 1) {
+                            echo "ja";
+                        } else {
+                            echo "nee";
+                        }
+                        ;
+                        ?>
+                    </td>
                     <td><a href="edit.php?id=<?php echo $ride['id']; ?>">aanpassen</a></td>
                 </tr>
             <?php endforeach; ?>
@@ -59,6 +109,8 @@ if(!isset($_SESSION['user_id']))
 
     </div>
 
+
+    
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -11,7 +11,9 @@ require_once 'admin/backend/config.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;600;700&family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;600;700&family=Roboto:wght@300;400;700&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
     <link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
     <link rel="icon" href="<?php echo $base_url; ?>/favicon.ico" type="image/x-icon" />
@@ -22,12 +24,156 @@ require_once 'admin/backend/config.php';
     <?php require_once 'header.php'; ?>
     <div class="container content">
         <aside>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia modi dolore magnam! Iste libero voluptatum autem, sapiente ullam earum nostrum sed magnam vel laboriosam quibusdam, officia, esse vitae dignissimos nulla?
+
+            <?php
+
+            require_once 'admin/backend/conn.php';
+
+            if (empty($_GET['theme']) && !isset($_GET['fastpass']) && empty($_GET['length']) && empty($_GET['name'])) {
+
+                $query = "SELECT * FROM rides";
+
+                $statement = $conn->prepare($query);
+
+                $statement->execute();
+
+            } else if (isset($_GET['theme'])) {
+
+                $query = "SELECT * FROM rides WHERE lower(themeland = :theme)";
+
+                $statement = $conn->prepare($query);
+
+                $statement->execute([":theme" => $_GET['theme']]);
+
+            } else if (isset($_GET['fastpass'])) {
+
+                $query = "SELECT * FROM rides WHERE lower(fast_pass = :fastpass)";
+
+                $statement = $conn->prepare($query);
+
+                $statement->execute([":fastpass" => $_GET['fastpass']]);
+
+            } else if (isset($_GET['length'])) {
+
+                $query = "SELECT * FROM rides WHERE lower(min_length <= :length) ORDER BY min_length DESC";
+
+                $statement = $conn->prepare($query);
+
+                $statement->execute([":length" => $_GET['length']]);
+
+            } else if (isset($_GET['name'])) {
+
+                $testName = $_GET['name'];
+
+                $query = "SELECT * FROM rides WHERE lower(title) LIKE '$testName%'";
+
+                $statement = $conn->prepare($query);
+
+                $statement->execute([":name" => $_GET['name']]);
+
+            }
+
+            $rides = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            ?>
+            <div class="dropdown">
+                <form action="" method="GET">
+                    <select name="fastpass">
+                        <option value="">Fast_pass nodig</option>
+                        <option value="1">ja</option>
+                        <option value="0">nee</option>
+                    </select>
+                    <input type="submit" value="filter">
+                </form>
+                <form action="" method="GET">
+                <select name="theme">
+                        <option value="">themagebied</option>
+                        <option value="familyland">familyland</option>
+                        <option value="adventureland">adventureland</option>
+                        <option value="waterland">waterland</option>
+                    </select>
+                    <input type="submit" value="filter">
+                </form> 
+                <form action="" method="GET">
+                <select name="length">
+                        <option value="">min length</option>
+                        <option value="200">langer dan 1 meter</option>
+                        <option value="100">korter dan 1 meter</option>
+                    </select>
+                    <input type="submit" value="filter">
+                </form>
+            </div>
+
         </aside>
         <main>
-            <!-- hier komen de attractiekaartjes -->
+
+            <div class="grid">
+                <?php foreach ($rides as $ride): ?>
+
+
+                    <?php if ($ride['fast_pass'] == 0): ?>
+                        <div class="kaart">
+
+                            <img src="img/attracties/<?php echo $ride['img_file']; ?>" height="200">
+                            <div class="kaarttekst">
+                                <h1>
+                                    <?php echo $ride['themeland']; ?>
+                                </h1>
+                                <h2>
+                                    <?php echo $ride['title']; ?>
+                                </h2>
+                                <p>
+                                    <?php echo $ride['description']; ?>
+                                </p>
+                                <?php if (isset($ride['min_length'])): ?>
+
+                                    <h3>
+                                        <?php echo $ride['min_length']; ?> cm minimale lengte
+                                    </h3>
+
+                                <?php endif; ?>
+                            </div>
+                        </div>
+
+                    <?php elseif ($ride['fast_pass'] == 1): ?>
+                        <div class="kaart2">
+                            <img src="img/attracties/<?php echo $ride['img_file']; ?>" height="200">
+                            <div class="kaart2divs">
+                                <div class="kaart2div1">
+                                    <h1>
+                                        <?php echo $ride['themeland']; ?>
+                                    </h1>
+                                    <h2>
+                                        <?php echo $ride['title']; ?>
+                                    </h2>
+                                    <p>
+                                        <?php echo $ride['description']; ?>
+                                    </p>
+                                    <?php if (isset($ride['min_length'])): ?>
+                                        <h3>
+                                            <?php echo $ride['min_length']; ?> cm minimale lengte
+                                        </h3>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="kaart2div2">
+                                    <div class="kaart2div2tekst1">
+                                        <p>Deze attractie is alleen te<br>bezoeken met een fastpass</p>
+                                    </div>
+                                    <div class="kaart2div2tekst2">
+                                        <p>Boek nu en sla de wachtrij over:</p>
+                                        <div class="kaart2div2tekst2img">
+                                            <button><img src="img/Ticket.png">FAST PASS</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+
+                <?php endforeach; ?>
+            </div>
         </main>
-    </div>
+
 
 </body>
 
